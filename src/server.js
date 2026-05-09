3c91464b22f7b9bb6b9433354cc825332123c0a4
Ruslan Kirvas <kirvas.ruslan@gmail.com>
2026-05-09 23:31:01 +0200

    debug: add environment diagnostics before server start

--- src/server.js
+++ src/server.js
@@ -40,4 +40,12 @@ const startServer = async () => {
   });
 };
 
+
+
+console.log("=== ENV CHECK ===");
+console.log("PORT:", process.env.PORT);
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("MONGO_URL:", process.env.MONGO_URL ? "defined" : "undefined");
+console.log("=================");
+
 startServer();

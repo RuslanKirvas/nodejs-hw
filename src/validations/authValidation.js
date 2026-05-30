eb50ea1b2bb01455c795128e2393825b8f8e4666
Ruslan Kirvas <kirvas.ruslan@gmail.com>
2026-05-30 15:10:46 +0200

    fix: correct sendMail syntax and add from field

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -20,7 +20,7 @@ export const loginUserSchema = {
 
 export const requestResetEmailSchema = {
     [Segments.BODY]: Joi.object({
-      email: Joi.string().min().email().required(),
+      email: Joi.string().email().required(),
 
   })
 }
@@ -30,7 +30,7 @@ export const requestResetEmailSchema = {
 export const resetPasswordSchema = {
     [Segments.BODY]: Joi.object({
 
-    password: Joi.string().min(8).required(),
+    password: Joi.string().min(6).required(),
    token: Joi.string().required(),
 
   })
